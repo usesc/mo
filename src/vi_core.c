ee6009378ebba459eab4b5d0733c7562313dac46
ignoremyaccount <jw90457@leisd.net>
2025-08-19 13:17:12 -0500

    Update vi_core.c

--- src/vi_core.c
+++ src/vi_core.c
@@ -23,8 +23,8 @@ ssize_t vi_insert(char *out, unsigned int ous, char *inp, unsigned int ins, unsi
 static inline __attribute__((always_inline, hot))
 ssize_t vi_delete(char *out, char *inp, unsigned int ins, unsigned int pos) {
 	vi_assert(out != NULL);
-  vi_assert(inp != NULL);
-  vi_assert(pos < ins);
+	vi_assert(inp != NULL);
+	vi_assert(pos < ins);
 
 	if (pos >= ins) return -1; 
 
@@ -33,6 +33,9 @@ ssize_t vi_delete(char *out, char *inp, unsigned int ins, unsigned int pos) {
 	
 	return ins-1;
 }
+
+/* vi delete two, TODO: */
+/* deletes memory from one off_t to another and shifts memory down  */
 
 /* vi init vi file */
 /* vif = vi file, fin = filename, fla = flags, mod = mode */
@@ -75,37 +78,51 @@ int vi_inivif(struct vi_file *vif, char *fin, int fla, mode_t mod) {
 /* vi free file */
 /* vif = vi file */
 void vi_freevif(struct vi_file *vif) {
-  if (!vif) return;
+	if (!vif) return;
 
-  if (vif->fim) {
-    free(vif->fim);
-    vif->fim = NULL;
-  }
+	if (vif->fim) {
+		free(vif->fim);
+    	vif->fim = NULL;
+  	}
 
-  if (vif->fid >= 0) {
-    close(vif->fid);
-    vif->fid = -1;
-  }
-
-  memset(vif, 0, sizeof(*vif));
+  	if (vif->fid >= 0) {
+    	close(vif->fid);
+    	vif->fid = -1;
+  	}
+	
+  	memset(vif, 0, sizeof(*vif));
 }
 
 /* vi increment until byte */
 /* mem = memory, mln = length, cha = char */
 static inline __attribute__((always_inline, hot))
 off_t vi_iub(const char *mem, size_t mln, char cha) {
-  for (off_t i = 0; i < mln; i++) {
-    if (mem[i] == cha) return i;
-  }
-  return -1; 
+	for (off_t i = 0; i < mln; i++) {
+		if (mem[i] == cha) return i;
+	}
+  	return -1; 
 }
 
 /* vi decrement until byte */
 /* mem = memory, off = offset, cha = char */
 static inline __attribute__((always_inline, hot))
 off_t vi_dub(const char *mem, off_t off, char cha) {
-  for (off_t i = off; i >= 0; i--) {
-    if (mem[i] == cha) return i;
-  }
-  return -1; 
+	for (off_t i = off; i >= 0; i--) {
+		if (mem[i] == cha) return i;
+	}
+	return -1; 
+}
+
+/* vi delete current line */
+/* mem = memory, off = offset, */
+static inline __attribute__((always_inline, hot))
+off_t vi_dcl(const char *mem, off_t off) {
+	/* 
+ 	calculate decrement until newline and
+ 	increment, store those two values as off_t
+  	buffer+offt1 = start of line
+   	buffer+offt2 = end of line
+	delete from start of line to end
+ 	shift memory back where it belongs
+ 	*/
 }
