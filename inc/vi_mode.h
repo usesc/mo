b275993210101eca34cecb2e49019d6c0d1f17a6
Anon <userca68@gmail.com>
2025-08-19 00:32:28 -0500

    alright im here to start modes

---
+++ inc/vi_mode.h
@@ -0,0 +1,24 @@
+
+/* vi normal mode controls */
+struct vi_normal_con {
+  int sti; /* switch to insert        */
+  int stt; /* switch to real terminal */
+  int stv; /* switch to vi terminal   */
+  int qts; /* quit this session       */
+};
+
+
+/* vi insert controls */
+struct vi_insert_con {
+
+};
+
+/* vi real terminal controls */
+struct vi_rterminal_con {
+
+};
+
+/* vi editor terminal controls */
+struct vi_terminal_con {
+
+};
