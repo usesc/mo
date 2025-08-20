441f00bc4073dea4020ee4460ed3438d73f7dfe2
ignoremyaccount <jw90457@leisd.net>
2025-08-20 11:25:39 -0500

    Update vi_core.c

--- src/vi_core.c
+++ src/vi_core.c
@@ -3,7 +3,7 @@
 /* REDESIGN TIME, INSTEAD OF INPUT BUFFER AND OUTPUT BUFFER, JUST USE MEMORY AND RETURN END SIZE */
 /* REPLACE VI_ASSERTS WITH VI_FAILS */
 
-/* vi insert */
+/* vi insert n bytes after offset */
 /* mm1 = memory 1, m1s = memory 1 size, mm2 = memory 2, m2s = memory 2 size, off = offset */
 static inline __attribute__((always_inline, hot))
 ssize_t vi_insert2(char *mm1, size_t m1s, char *mm2, size_t m2s, size_t off) {
@@ -16,7 +16,8 @@ ssize_t vi_insert2(char *mm1, size_t m1s, char *mm2, size_t m2s, size_t off) {
 	
 	return m1s+m2s;
 }
-/* vi delete */
+
+/* vi delete n bytes after offset */
 /* mem = memory, msz = memory size, off = offset, byt = N bytes */
 static inline __attribute__((always_inline, hot))
 ssize_t vi_delete(char *mem, size_t msz, size_t off, size_t byt) {
@@ -66,9 +67,61 @@ int vi_inivif(struct vi_file *vif, char *fin, int fla, mode_t mod) {
 	return 0;
 }
 
+int vi_inivif_mmap(struct vi_file *vif, char *fin, int fla, mode_t mod) {
+		vif->fin = fin;
+	vif->fns = vi_strlen(fin);
+
+	vif->fid = open(fin, fla, mod);
+	if (vif->fid  == -1) {
+		vi_errors("open");
+		return -1;
+	}
+
+	if (fstat(vif->fid , &vif->sts) == -1) {
+		vi_errors("fstat");
+		close(vif->fid);
+		return -1;
+	}
+
+	vif->fml = vif->sts.st_size;
+
+	vif->fim = mmap();
+	if (!vif->fim) {
+		vi_errors("malloc");
+		close(vif->fid);
+		return -1;
+	}
+
+	ssize_t n = read(vif->fid, vif->fim, vif->fml);
+	if (n != vif->fml) {
+		vi_errors("read");
+		close(vif->fid);
+		free(vif->fim);
+		return -1;
+	}
+	
+	return 0;
+}
+
 /* vi free file */
 /* vif = vi file */
 void vi_freevif(struct vi_file *vif) {
+	if (!vif) return;
+
+	if (vif->fim) {
+		free(vif->fim);
+    	vif->fim = NULL;
+  	}
+
+  	if (vif->fid >= 0) {
+    	close(vif->fid);
+    	vif->fid = -1;
+  	}
+	
+  	memset(vif, 0, sizeof(*vif));
+}
+
+void vi_freevif_mmap(struct vi_file *vif) {
 	if (!vif) return;
 
 	if (vif->fim) {
